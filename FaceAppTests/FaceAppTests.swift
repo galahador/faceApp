1910a0fda475dfd29a9f2949508a92f58f75910a
tBug <galahador@protonmail.com>
2024-10-26 21:26:03 +0200

    Main first Commit

---
+++ FaceAppTests/FaceAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAppTests.swift
+//  FaceAppTests
+//
+//  Created by tBug on 26.10.24..
+//
+
+import Testing
+@testable import FaceApp
+
+struct FaceAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
